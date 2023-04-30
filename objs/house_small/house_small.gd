4e632522d5401fa8ff90f119dd2367b2e8bac0b6
Matt Swieboda <mswieboda@users.noreply.github.com>
2023-04-30 14:16:59 -0500

    house hover refactor

--- objs/house_small/house_small.gd
+++ objs/house_small/house_small.gd
@@ -2,8 +2,14 @@ extends Node3D
 
 var selection_material_overlay = preload("res://assets/3d/houses/selection_material_overlay_3d.tres")
 
+var is_hovered = false
+
 func _on_selection_area_mouse_entered():
-  $mesh/Cube.material_overlay = selection_material_overlay
+  hover(true)
 
 func _on_selection_area_mouse_exited():
-  $mesh/Cube.material_overlay = null
+  hover(false)
+
+func hover(hover: bool):
+  $mesh/Cube.material_overlay = selection_material_overlay if hover else null
+  is_hovered = hover
