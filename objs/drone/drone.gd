dae86bb71c691a596a8f613860c94facdbe4ad27
Matt Swieboda <mswieboda@users.noreply.github.com>
2023-04-30 01:00:48 -0500

    drones come out of warehouse and back in

--- objs/drone/drone.gd
+++ objs/drone/drone.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 const FLY_HEIGHT = 10
-const SPEED = 500
+const SPEED = 666
 const DISTANCE_THRESHOLD = 0.3
 
 var destination_position : Vector3 = Vector3.ZERO
@@ -9,9 +9,8 @@ var box_to_pickup : Node3D = null
 var box_to_deliver : Node3D = null
 var deliver_box_location : Node3D = null
 var go_to_height = false
-
-func _ready():
-  transform.origin.y = FLY_HEIGHT
+var go_to_warehouse = false
+@onready var drone_spawn_location = get_parent().get_parent().get_node('warehouse/drone_spawn_location')
 
 func _physics_process(delta):
   movement(delta)
@@ -23,17 +22,17 @@ func movement(delta):
   movement_pickup(delta)
   movement_deliver(delta)
   movement_go_to_height(delta)
+  movement_go_to_warehouse(delta)
   move_and_slide()
 
-func movement_pickup(delta):
-  if !box_to_pickup:
-    return
-
+func move_laterally(delta):
   var horz_position = global_position
   horz_position.y = destination_position.y
 
   if horz_position.x != destination_position.x && horz_position.z != destination_position.z:
-    look_at(destination_position, Vector3.UP)
+    var look_at_position = destination_position
+    look_at_position.y = global_position.y
+    look_at(look_at_position, Vector3.UP)
 
   if horz_position.distance_to(destination_position) < DISTANCE_THRESHOLD:
     # lock into dest x/z since we're close enough
@@ -47,34 +46,46 @@ func movement_pickup(delta):
   if direction:
     velocity.x = direction.x * SPEED * delta
     velocity.z = direction.z * SPEED * delta
+    return false
   else:
     velocity.x = move_toward(velocity.x, 0, SPEED * delta)
     velocity.z = move_toward(velocity.z, 0, SPEED * delta)
+    return true
 
-    # reached box x,z
-    if $box_location.global_position.distance_to(box_to_pickup.global_position) < DISTANCE_THRESHOLD:
-      # pickup box and prep to go to height
-      box_to_deliver = box_to_pickup
-      box_to_deliver.reparent($box_location)
-      box_to_pickup = null
-      go_to_height = true
-      destination_position = global_position
-      destination_position.y = global_position.y + FLY_HEIGHT
-      return
-    else:
-      # go down to box
-      direction = (box_to_pickup.global_position - global_position).normalized()
+func movement_pickup(delta):
+  if go_to_height or !box_to_pickup:
+    return
 
-    if direction:
-      velocity.y = direction.y * SPEED * delta
-    else:
-      velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+  if !move_laterally(delta):
+    return
+
+  # reached box x,z
+  if $box_location.global_position.distance_to(box_to_pickup.global_position) < DISTANCE_THRESHOLD:
+    # pickup box and prep to go to height
+    box_to_deliver = box_to_pickup
+    box_to_deliver.reparent($box_location)
+    box_to_pickup = null
+    go_to_height = true
+    destination_position = global_position
+    destination_position.y = global_position.y + FLY_HEIGHT
+    return
+
+  move_down_to(box_to_pickup, delta)
+
+func move_down_to(node, delta):
+  var direction = (node.global_position - global_position).normalized()
+
+  if direction:
+    velocity.y = direction.y * SPEED * delta
+  else:
+    velocity.y = move_toward(velocity.y, 0, SPEED * delta)
 
 func movement_go_to_height(delta):
   if !go_to_height:
     return
 
-  if global_position.distance_to(destination_position) < DISTANCE_THRESHOLD:
+  var dist = global_position.distance_to(destination_position)
+  if dist < DISTANCE_THRESHOLD:
     global_position = destination_position
 
   var direction = (destination_position - global_position).normalized()
@@ -85,10 +96,14 @@ func movement_go_to_height(delta):
     velocity.y = move_toward(velocity.y, 0, SPEED * delta)
     go_to_height = false
 
-    if deliver_box_location:
-      # finished delivering a box, and reached fly height, reset so we can go back to warehouse
+    if box_to_pickup:
+      destination_position = box_to_pickup.global_position
+      destination_position.y = global_position.y
+    elif deliver_box_location:
+      # finished delivering a box, and reached fly height, go back to warehouse
       deliver_box_location = null
-      destination_position = Vector3()
+      go_to_warehouse = true
+      destination_position = drone_spawn_location.global_position
 
 func movement_deliver(delta):
   if go_to_height or !box_to_deliver:
@@ -105,52 +120,41 @@ func movement_deliver(delta):
     destination_position = box_location.global_position
   else:
     # found a deliver_box_location, move x,z there
-    var horz_position = global_position
-    horz_position.y = destination_position.y
+    if !move_laterally(delta):
+      return
 
-    if horz_position.x != destination_position.x && horz_position.z != destination_position.z:
-      var look_at_position = destination_position
-      look_at_position.y = global_position.y
-      look_at(look_at_position, Vector3.UP)
+    # reached box x,z
+    if $box_location.global_position.distance_to(deliver_box_location.global_position) < DISTANCE_THRESHOLD:
+      # deliver box and prep to go to height
+      box_to_deliver.reparent(deliver_box_location)
+      box_to_deliver = null
+      go_to_height = true
+      destination_position = global_position
+      destination_position.y = global_position.y + FLY_HEIGHT
+      return
 
-    if horz_position.distance_to(destination_position) < DISTANCE_THRESHOLD:
-      # lock into dest x/z since we're close enough
-      horz_position.x = destination_position.x
-      horz_position.z = destination_position.z
-      global_position.x = destination_position.x
-      global_position.z = destination_position.z
+    move_down_to(deliver_box_location, delta)
 
-    var direction = (destination_position - horz_position).normalized()
+func movement_go_to_warehouse(delta):
+  if go_to_height or !go_to_warehouse:
+    return
 
-    if direction:
-      velocity.x = direction.x * SPEED * delta
-      velocity.z = direction.z * SPEED * delta
-    else:
-      velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-      velocity.z = move_toward(velocity.z, 0, SPEED * delta)
+  if !move_laterally(delta):
+    return
 
-      # reached box x,z
-      if $box_location.global_position.distance_to(deliver_box_location.global_position) < DISTANCE_THRESHOLD:
-        # deliver box and prep to go to height
-        box_to_deliver.reparent(deliver_box_location)
-        box_to_deliver = null
-        go_to_height = true
-        destination_position = global_position
-        destination_position.y = global_position.y + FLY_HEIGHT
-        return
-      else:
-        # go down to box
-        direction = (deliver_box_location.global_position - global_position).normalized()
+  # reached warehouse drone spawn location x,z
+  if global_position.distance_to(drone_spawn_location.global_position) < DISTANCE_THRESHOLD:
+    # mark for destruction
+    queue_free()
+    return
 
-      if direction:
-        velocity.y = direction.y * SPEED * delta
-      else:
-        velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+  move_down_to(drone_spawn_location, delta)
 
 func pickup_box(box):
   if destination_position != Vector3.ZERO or box_to_pickup or box_to_deliver or deliver_box_location or go_to_height:
     return false
 
+  destination_position = global_position
+  destination_position.y = FLY_HEIGHT
+  go_to_height = true
   box_to_pickup = box
-  destination_position = box_to_pickup.global_position
-  destination_position.y = global_position.y
--- objs/warehouse/warehouse.gd
+++ objs/warehouse/warehouse.gd
@@ -1,14 +1,47 @@
 extends Node3D
 
+
 var box_scene = preload("res://objs/box/box.tscn")
+var drone_scene = preload("res://objs/drone/drone.tscn")
+
+var MIN_BOX_TIMER = 3
+var MAX_BOX_TIMER = 15
+var INITIAL_MAX_DRONES = 3
+var max_drones = INITIAL_MAX_DRONES
+@onready var drones_node = get_parent().get_node('drones')
 
 func spawn_box():
+  if $box_location/spawn.get_child_count() > 0:
+    return
+
   var box = box_scene.instantiate()
 
   $box_location/spawn.add_child(box)
 
-func _on_initial_box_timer_timeout():
-  spawn_box()
+func spawn_drone():
+  if drones_node.get_child_count() >= max_drones:
+    return
+
+  if $box_location/spawn.get_child_count() <= 0:
+    return
+
+  var box = $box_location/spawn.get_child(0)
+
+  if !box or drones_node.get_children().any(func(drone): return drone.box_to_pickup == box):
+    return
+
+  var drone = drone_scene.instantiate()
+
+  get_parent().get_node('drones').add_child(drone)
+
+  drone.global_position = $drone_spawn_location.global_position
+  drone.pickup_box(box)
 
 func _on_box_timer_timeout():
+  $box_timer.stop()
+  $box_timer.start(randf_range(MIN_BOX_TIMER, MAX_BOX_TIMER))
   spawn_box()
+
+
+func _on_drone_timer_timeout():
+  spawn_drone()
