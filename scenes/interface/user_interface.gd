495878362b450c3773a64436c0e3a14d2315f9f6
Ben Eng <whysguy52@gmail.com>
2023-05-01 02:18:22 -0500

    available def drones counter fixed

--- scenes/interface/user_interface.gd
+++ scenes/interface/user_interface.gd
@@ -11,4 +11,5 @@ func update_ui():
 
 func update_def_ui():
   $h_info_container/max_def_drone_value.text = str(warehouse.max_def_drones)
-  $h_info_container/available_value.text = str(warehouse.max_def_drones - warehouse.deployed_def_drones_count() )
+  print("deployed drone count: ",warehouse.deployed_def_drones_count())
+  $h_info_container/available_def_drone_value.text = str(warehouse.max_def_drones - warehouse.deployed_def_drones_count())
